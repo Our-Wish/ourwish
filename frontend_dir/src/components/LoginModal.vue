507fc626b561c8185d020bbf073734755bcec0db
xxj15 <seojini00@naver.com>
2026-06-09 00:16:33 +0900

    Chore: alert 텍스트 메시지 변경

--- frontend_dir/src/components/LoginModal.vue
+++ frontend_dir/src/components/LoginModal.vue
@@ -81,7 +81,7 @@ const onSubmit = async () => {
 
   try {
     await authStore.login(email.value, password.value)
-    alert('로그인되었습니다!')
+    alert('로그인 성공!')
     emit('close')
   } catch {
     errorMessage.value = '로그인에 실패했습니다. 아이디와 비밀번호를 확인해 주세요.'
