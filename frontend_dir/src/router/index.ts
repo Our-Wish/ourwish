cdd9edb726a0b0614ff82474a22fd389c37d6e59
xxj15 <seojini00@naver.com>
2026-06-13 15:36:40 +0900

    Chore: 페이지 이동 시 스크롤 상단 이동 설정

--- frontend_dir/src/router/index.ts
+++ frontend_dir/src/router/index.ts
@@ -6,6 +6,7 @@ import SavingsDetailView from '@/views/SavingsDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior: () => ({ top: 0 }),
   routes: [
     {
       path: '/',
