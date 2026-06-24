6467d2f3c849e247c4f859c1fb342d139523f11e
xxj15 <seojini00@naver.com>
2026-06-24 16:31:46 +0900

    Fix: 금융TV 초기 자동 검색 복구

--- frontend_dir/src/components/FinanceLounge/VideoSection.vue
+++ frontend_dir/src/components/FinanceLounge/VideoSection.vue
@@ -89,7 +89,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import api from '@/api'
 import VideoCard from '@/components/Video/VideoCard.vue'
 import { decodeHtmlEntities } from '@/utils/decodeHtml'
@@ -149,4 +149,7 @@ function search() {
   runSearch(query.value)
 }
 
+onMounted(() => {
+  runSearch('예적금')
+})
 </script>
